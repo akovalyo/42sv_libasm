4c8bd6f7b80fc86968453f364871e647bf961191
Alexandr Kovalyov <al.kovalyov@gmail.com>
2020-07-30 12:20:29 -0700

    add ft_atoi_base

--- ft_atoi_base_bonus.s
+++ ft_atoi_base_bonus.s
@@ -6,49 +6,49 @@ _ft_atoi_base:
 				xor		r9b, r9b			; int sign = 0
 				xor		r10b, r10b			; flag = 0
 				xor		r11, r11			; collector = 0
-				;push	rdi					; save str
-				;mov		rdi, rsi		; rdi = base
 				xor		rcx, rcx			; i = 0
-				jmp 	atoi_skip_ident
+				jmp		atoi_skip_ident
 
 atoi_ident_inc:
 				inc		rcx					; i++
 
 atoi_skip_ident:
-				cmp		byte[rsi + rcx], 32
+				mov		dl, byte[rsi + rcx]
+				cmp		dl, 32				; if == ' '
 				jz		atoi_skip_ident
-				cmp		byte[rsi + rcx], 9
+				cmp		dl, 9				; if == '\t'
 				jz		atoi_skip_ident
-				cmp		byte[rsi + rcx], 10
+				cmp		dl, 10 				; if == '\n'
 				jz		atoi_skip_ident
-				cmp		byte[rsi + rcx], 11
+				cmp		dl, 11 				; if == '\v'
 				jz		atoi_skip_ident
-				cmp		byte[rsi + rcx], 12
+				cmp		dl, 12 				; if == '\f'
 				jz		atoi_skip_ident
-				cmp		byte[rsi + rcx], 13
+				cmp		dl, 13 				; if == '\r'
 				jz		atoi_skip_ident
 				cmp		r10b, 1				; if flag == 1 
 				jz		check_sign
 
 base_invalid:
-				cmp		byte[rsi + rcx], 45	; base[i] == '-'
+				mov		dl, byte[rsi + rcx]
+				cmp		dl, 45				; base[i] == '-'
 				jz		ret_inv
-				cmp		byte[rsi + rcx], 43	; base[i] == '+'
+				cmp		dl, 43				; base[i] == '+'
 				jz		ret_inv
-				cmp		byte[rsi + rcx], 48 ; base[i] == '0'
+				cmp		dl, 48 				; base[i] == '0'
 				jz		ret_inv
 
 atoi:
 				mov		dl, byte[rsi + rcx]
-				cmp		dl, 0
+				cmp		dl, 0				; check the end of string
 				jz		check_base_int
+											; collector *= 10 + base[i] - '0'	
 				sub		dl, 48
 				movzx	rax, dl
 				imul	r8, 10
 				add		r8, rax
 				inc		rcx
-				jmp 	atoi
-
+				jmp		atoi
 
 check_base_int:
 				cmp		r8, 17
@@ -87,19 +87,13 @@ char_to_int:
 				movzx	r12, dl
 				sub		r12, 65
 				cmp		r12, 5				; if r12 <= 4
-				jle		convert_large_letter
+				jle		convert_letter
 				movzx	r12, dl
 				sub		r12, 97
 				cmp		r12, 5				; if r12 <= 4
-				jle		convert_large_letter
+				jle		convert_letter
 
-convert_large_letter:
-				cmp		r12, -1
-				jle		return
-				add		r12, 10
-				jmp		check_base	
-
-convert_small_letter:
+convert_letter:
 				cmp		r12, -1
 				jle		return
 				add		r12, 10
@@ -133,7 +127,7 @@ return:
 				ret
 
 ret_neg:
-				neg 	rax
+				neg		rax
 				ret
 
 ret_inv:
